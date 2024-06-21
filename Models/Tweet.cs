6427ebddc32e8d374459ed5a5e43a61acf4e9b32
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:30:20 +0530

    Create Tweet.cs

---
+++ Models/Tweet.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterCloneAPI.Models
+{
+    public class Tweet
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(280)]
+        public string Text { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+        public User User { get; set; }
+
+        public string Content { get; set; } 
+        public List<string> LikedByUsers { get; set; }
+        public int LikeCount { get; internal set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
