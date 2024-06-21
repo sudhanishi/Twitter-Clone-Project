d8356c3d7359c5a70e54f9f3cc1325f904b85c7f
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:39:36 +0530

    Create LoginViewModel.cs

---
+++ _ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "The Email field is required.")]
+    [EmailAddress(ErrorMessage = "The Email field is not a valid email address.")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "The Password field is required.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [Display(Name = "Remember me")]
+    public bool RememberMe { get; set; }
+}
