12151fdcef196815a059f12df33c4c46c1c0211b
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:40:27 +0530

    Create RegisterViewModel.cs

---
+++ _ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "The Email field is required.")]
+    [EmailAddress(ErrorMessage = "The Email field is not a valid email address.")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "The Password field is required.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 6)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPassword { get; set; }
+}
