c693395dbbb94635e4db864bf16f5d6a60e66cb5
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:30:50 +0530

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitterCloneAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        // Add other user properties as needed
+        public ICollection<Tweet> Tweets { get; set; }
+    }
+}
