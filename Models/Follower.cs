aa8b0aba7324a835f7d21bc69cc59917e7562926
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:26:28 +0530

    Create Follower.cs

---
+++ Models/Follower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TwitterCloneAPI.Models
+{
+    public class Follower
+    {
+        public string FollowerId { get; set; }
+        public ApplicationUser FollowerUser { get; set; }
+
+        public string FolloweeId { get; set; }
+        public ApplicationUser FolloweeUser { get; set; }
+    }
+}
