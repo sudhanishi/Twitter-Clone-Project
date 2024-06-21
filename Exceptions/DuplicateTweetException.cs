b3154c30adcd40699e0cde6144c2959e4293ac1f
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:10:53 +0530

    Create DuplicateTweetException.cs

---
+++ Exceptions/DuplicateTweetException.cs
@@ -0,0 +1,12 @@
+﻿// DuplicateTweetException.cs
+using System;
+
+namespace TwitterCloneAPI.Exceptions
+{
+    public class DuplicateTweetException : Exception
+    {
+        public DuplicateTweetException(string message) : base(message)
+        {
+        }
+    }
+}
