035a3014865d8c76844850863885087e0ca54e87
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:28:35 +0530

    Create Model.cs

---
+++ Models/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitterCloneAPI.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string UserId { get; set; } // ID of the user who posted the comment
+        public ApplicationUser User { get; set; } // Navigation property to the user
+    }
+}
