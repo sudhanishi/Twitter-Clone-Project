cf9fcfa0254ff44d48841b6d549b7193ec1f3ccf
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:25:07 +0530

    Create ApplicationUser.cs

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+public class ApplicationUser : IdentityUser
+{
+    // User properties
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ProfilePictureUrl { get; set; }
+    public string Bio { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    // Navigation property for followers
+    public ICollection<ApplicationUser> Followers { get; set; }
+
+    // Navigation property for users the current user is following
+    public ICollection<ApplicationUser> Following { get; set; }
+
+    // Additional properties can be added here
+
+    // Navigation properties
+    // For example, if a user has tweets, you can define a navigation property like this:
+    // public ICollection<Tweet> Tweets { get; set; }
+}
