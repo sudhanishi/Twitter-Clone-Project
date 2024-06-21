a2e90007c04de19e5ab5404bae73f192b4360f8c
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:31:29 +0530

    Create UserProfile.cs

---
+++ Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitterCloneAPI.Models
+{
+    // UserProfile.cs
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string Bio { get; set; }
+        // Add other properties as needed
+    }
+}
