2b16ebd4a90d70e2cb565c7562e676c1a44e15f2
sudhanishi <165028075+sudhanishi@users.noreply.github.com>
2024-06-21 16:29:14 +0530

    Create RegisterModel.cs

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RegisterModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+    public string FirstName { get; internal set; }
+    public string LastName { get; internal set; }
+    public DateTime BirthDate { get; internal set; }
+    public string ProfilePictureUrl { get; internal set; }
+    public string Bio { get; internal set; }
+}
